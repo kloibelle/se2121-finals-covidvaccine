396c3905c7e872d32da29fe9d2377095d78c41c8
Chloe Belle ESTILO <70317267+kloibelle@users.noreply.github.com>
2022-01-07 23:06:35 +0800

    Edited datatype parameters
    Edited datatypes and some parameters

--- covid_db.sql
+++ covid_db.sql
@@ -1,29 +1,28 @@
 CREATE DATABASE covid_db;
 
 CREATE TABLE locations(
-    loc_name VARCHAR(90) NOT NULL,
-    loc_iso_code INT PRIMARY KEY
-)
+    loc_name VARCHAR(2000) NOT NULL,
+    loc_iso_code VARCHAR(60) PRIMARY KEY
+);
 
-CREATE TABLE vaccines(
-    vac_name VARCHAR(50) PRIMARY KEY
-) 
+CREATE TABLE vaccines (
+    vac_name VARCHAR(2000) PRIMARY KEY
+);
 
 CREATE TABLE vaccinations_by_manufacturer(
-
     vbm_id BIGSERIAL PRIMARY KEY,
-    vbm_location VARCHAR(90) NOT NULL,
+    vbm_location VARCHAR(2000) NOT NULL,
     vbm_date DATE NOT NULL,
-    vbm_vaccine VARCHAR(50) NOT NULL
+    vbm_vaccine VARCHAR(1000) NOT NULL,
     vbm_total_vaccinations BIGINT NOT NULL
-)
+);
 
 CREATE TABLE vaccines_by_location(
     vbl_id BIGSERIAL PRIMARY KEY,
-    vbl_location VARCHAR(90) NOT NULL,
-    vbl_iso_code VARCHAR(60) NOT NULL,
+    vbl_location VARCHAR(1000) NOT NULL,
+    vbl_iso_code VARCHAR(1000) NOT NULL,
     vbl_vaccines TEXT NOT NULL,
     vbl_last__observation_date DATE NOT NULL,
-    vbl_source_name VARCHAR(80) NOT NULL
+    vbl_source_name VARCHAR(1000) NOT NULL,
     vbl_source_website VARCHAR(2048)
-)
+);
